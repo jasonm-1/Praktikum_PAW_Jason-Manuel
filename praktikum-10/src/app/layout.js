acd0877cc2cc9297a8e857ed100611be318c019d
jasonm-1 <jasonmanuel1023@gmail.com>
2026-06-04 19:48:52 +0700

    Syntax Kode Tugas Praktikum 1

---
+++ praktikum-10/src/app/layout.js
@@ -0,0 +1,16 @@
+import "./globals.css";
+import { AuthProvider } from "@/components/AuthProvider";
+import Navbar from "@/components/Navbar"; 
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthProvider>
+          <Navbar />
+          {children}
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
